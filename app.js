997a047aabc76f54d727c187554cade9bb0c4d07
Harun Cimen <haruncimen@gmail.com>
2022-12-07 13:37:17 -0500

    script moved in to app.js file

--- app.js
+++ app.js
@@ -1,12 +1,47 @@
-function my1(x){
-    z.value+=x;
-    }
-
-function cls(){
-    z.value="";}
+let x=document.getElementById("text");
+    
+function my1(v){
+x.value+=v;
+}
 
 function exe(){
-    let z = document.getElementById("text").value
-    let y = eval(z)
-    document.getElementById("text").value= y
-    }
+x.value=eval(x.value);
+}
+
+function cls(){
+x.value="";
+}
+
+function cancel(){
+x.value=x.value.substr(0,x.value.length-1);
+}
+
+function percent(){
+x.value = eval(x.value)  * 1/100 ;
+}
+
+function exponent(){
+x.value=eval(x.value);
+x.value = eval(x.value * x.value);
+}
+
+function pi(){
+var π = 3.1415;
+x.value+=π;
+}
+
+function permille(){
+x.value = eval(x.value)  * 1/1000 ;
+}
+
+function sqrt(){
+x.value = eval(Math.sqrt(x.value)) ;
+}
+
+function minus(){
+x.value = eval(x.value)  * -1 ;
+}
+
+function abs(){
+x.value = eval(Math.abs(x.value)) ;
+}
